7f4e04a062b2873231ff05b3b8016b79b9088b49
develowlper <dev@develowlper.de>
2020-04-29 00:57:16 +0200

    feat(app):  @storybook/addon-essential

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs',
